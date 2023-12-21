560fcfbbe4df285cb44651e58c5671df2abdd408
partho.singularity <partho.singularity@gmail.com>
2023-12-21 11:25:00 +0600

    add comment in request validation class

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\Backend\Payment\PaymentController;
-use App\Http\Controllers\Backend\AuthController;
+
 
 /*
 |--------------------------------------------------------------------------
